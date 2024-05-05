6ce2169979388500d62b0a1259d85de98fab14f1
Nadun Madawala <nmadawala96@gmail.com>
2024-05-05 19:47:45 +0530

    basic basement

--- RPS game.py
+++ RPS game.py
@@ -1,1 +1,16 @@
 import random
+
+user_wins = 0
+computer_wins = 0
+
+while True:
+    user_input = input("Type Rock/Paper/Scissor or Q to quit.").lower()
+    if user_input == 'q':
+        quit()
+
+    if user_input in ["rock", "paper", "scissor"]:
+        continue
+
+    random_number = random.randint(0, 2)
+
+print("Goodbye!")
